66703ae45ac2586590650d40a1cf3d15c12f947e
Julian Galvez Serna <julian.galvez.s@gmail.com>
2019-01-29 17:29:14 -0500

    Integración Codigo Estacion y Camara
    Se integro el codigo para la captura de datos de la estacion metereleogica dentro del codigo de captura de las imagenes

--- CamaraCielo/allSky/allSkyVLC.py
+++ CamaraCielo/allSky/allSkyVLC.py
@@ -1,8 +1,17 @@
 
 #!/bin/python
 
-###################################### Parametros Codigo ##################################################
 
+###################################### Librerias ##################################################
+import os
+from time import gmtime, strftime, localtime, strptime, time, sleep#, timedelta
+import RPi.GPIO as GPIO
+import gps as gp
+import threading
+import serial
+import csv
+
+###################################### Parametros Codigo ##################################################
 # Parametros codigo:
 tcap = 30 # Tiempo caputura fotogramas en segundos
 tencap = 30 # Tiempo entre capturas en segundos
@@ -10,17 +19,24 @@ tencap = 30 # Tiempo entre capturas en segundos
 # Contador capturas
 rutaCon = "/home/pi/allSky/conteo.txt"
 rutaLog = "/home/pi/allSky/logSkyCam.txt"
+rutaLogD = "/home/pi/allSky/davisLog/DavisLog.txt"
 
-###################################### Librerias ##################################################
+Nombres = ['L','O','O','Bar Trend','Packet Type','Next Record','Barometer','Inside Temperature','Inside Humidity','Outside Temperature','Wind Speed','10 Min Avg Wind Speed','Wind Direction','Extra Temperatures','Soil Temperatures','Leaf Temperatures','Outside Humidity','Extra Humidties','Rain Rate','UV','Solar Radiation','Storm Rain','Start Date of current Storm','Day Rain','Month Rain','Year Rain','Day ET','Month ET','Year ET','Soil Moistures','Leaf Wetnesses','Inside Alarms','Rain Alarms','Outside Alarms','Extra Temp/Hum Alarms','Soil & Leaf Alarms','Transmitter Battery Status','Console Battery Voltage','Forecast Icons','Forecast Rule number','Time of Sunrise','Time of Sunset','<LF> = 0x0A','<CR> = 0x0D','CRC']
+Doff    = [1, 2, 3, 4, 5, 6, 8, 10, 12, 13, 15, 16, 17, 19, 26, 30, 34, 35, 42, 44, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 67, 71, 72, 73, 75, 83, 87, 88, 90, 91, 92, 94, 96, 97, 98]
+Dsize   = [1, 1, 1, 1, 1, 2, 2, 2, 1, 2, 1, 1, 2, 7, 4, 4, 1, 7, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 4, 1, 1, 2, 8, 4, 1, 2, 1, 1, 2, 2, 1, 1, 2]
+DFact   = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]  
+DSave   = [0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+TamD = len(Nombres)
 
-import os
-from time import gmtime, strftime, localtime, strptime, time, sleep#, timedelta
-import RPi.GPIO as GPIO
-import gps as gp
-import threading
+###################################### Variables ##################################################                
+# variables captura tiempo 
+
+tiempoS = [2000, 0, 0, 0, 0, 0]
+tiempoG = [2000, 0, 0, 0, 0, 0]
+tiempo = [2000, 0, 0, 0, 0, 0]
+stopT1 = False
 
 ###################################### Puertos ###################################### 
-
 led_rojo = 19
 led_amar = 13
 led_verd = 6
@@ -32,15 +48,48 @@ GPIO.setup(led_rojo, GPIO.OUT)
 GPIO.setup(led_amar, GPIO.OUT)
 GPIO.setup(led_verd, GPIO.OUT)
 GPIO.setup(ena_easy, GPIO.OUT)
-                
-######################################  variables captura tiempo 
 
-tiempoS = [2000, 0, 0, 0, 0, 0]
-tiempoG = [2000, 0, 0, 0, 0, 0]
-tiempo = [2000, 0, 0, 0, 0, 0]
-stopT1 = False
+ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=5)
+print(ser.name)
+
 
 ###################################### Funciones ##################################################
+def mix2bytes(datosE,pos):
+  return ord(datosE[pos + 1]) * 256 + ord(datosE[pos])
+
+# Registro suscesos en Txt y consola
+def regLog(texto):
+    print (texto)
+    fileL = open(rutaLog, "a")   # se crea el archivo 
+    fileL.write(texto + '\n') 
+    fileL.close() 
+
+# Verificacion rutas guardado archivos
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+# Actualizamos tiempo sistema
+def actualizarTiempo():
+    # Extraemos tiempo sistema
+    tiempoS[0] = int(strftime("%Y", localtime()))
+    tiempoS[1] = int(strftime("%m", localtime()))
+    tiempoS[2] = int(strftime("%d", localtime()))
+    tiempoS[3] = int(strftime("%H", localtime()))
+    tiempoS[4] = int(strftime("%M", localtime()))
+    tiempoS[5] = int(strftime("%S", localtime()))
+
+    # Tomamos el mayor de los tiempos entre Sistema y GPS
+    for i in range(0,6):
+        if tiempoS[i] < tiempoG[i]: # tiempo sistema menor que gps, actualizamos
+            tiempo = tiempoG
+            return tiempo
+            break
+        if tiempoS[i] > tiempoG[i]: # tiempo sistema mayor que gps, actualizamos
+            tiempo = tiempoS
+            return tiempo
+            break
 
 # hilo de captura tiempo GPS
 def worker():
@@ -77,24 +126,88 @@ def worker():
             print "GPSD has terminated"
     return
 
+# hilo Captura datos Estacion
+def capturaEstacion():
+    ###################################### Guardamos Datos Estacion
+    while(1):
+
+        tiempo = actualizarTiempo()
+
+        regLog('Solicitando LOOP: ')
+        ser.write(b'\n')
+        ser.write(b'\n')
+        sleep(0.1)
+        ser.write(b'LOOP 1') 
+        x = ser.read(100)          # read 99 bytes
+        regLog('Lectura: ')
+        regLog(x)
+
+        # Creamos sistema de archivos
+        ruta = '/home/pi/allSky/davisLog/A' + str(tiempo[0]) + 'M' + "%02d"%tiempo[1] + '/'
+        ensure_dir(ruta)
+
+        # Se carga el tiempo
+        nombreArchivo = 'DatosEstacion' + str(tiempo[0]) + '-' + "%02d"%tiempo[1] + '-' + "%02d"%tiempo[2] 
+        tiempoStr = str(tiempo[0]) + ':' + "%02d"%tiempo[1] + ':' + "%02d"%tiempo[2]  + '-' + "%02d"%tiempo[3] + ':' + "%02d"%tiempo[4] + ':' + "%02d"%tiempo[5] 
+        regLog("nCaptura... " + ruta + " T: " + tiempoStr)
+
+        #Procesamos los datos Capturados
+        #regLog('Procesando Datos: ')
+        #regLog('# Datos Capturados' + str(len(x)))
+        #xe = x.encode('ASCII') 
+        Datos = [tiempoStr]     # Cargamos el tiempo como primera columna
+        if len(x) > 99:
+            for i in range(TamD):
+                aux = ''
+                #regLog(Nombres[i] + ' Save:' +  str(DSave[i]) + ' * [' + str(DFact[i])+ ']')
+                if i<4:
+                    aux = x[i] 
+                if Dsize[i] < 2:
+                    aux = str(ord(x[Doff[i]]))
+                else:
+                    aux = str(mix2bytes(x,Doff[i]))
+                #regLog(aux)
+                # Agregamos Datos a salida
+                if DSave[i] == 1:
+                    Datos.append(aux)
+        else:
+            regLog('Problema Captura')
+        
+
+        #Guardamos los datos Capturados
+        fileName = ruta + nombreArchivo + '.csv'
+        try:
+            #regLog('Abriendo archivo...' + fileName)
+            open(fileName, 'rb')
+        except:
+            # Archivo no existe, lo creamos
+            print('Error apertura... Creando archivo')
+            writer = csv.writer(open(fileName, 'w'))
+            # Cargamos nombres headers
+            headers = ['Tiempo Sistema'] # Cargamos el tiempo como primera columna
+            for i in range(TamD):
+                if DSave[i] == 1:
+                    headers.append(Nombres[i])
+            regLog('Cabecera a escribir: ' + str(headers))
+            writer.writerow(headers)
+
+        writer = csv.writer(open(fileName, 'a'))
+        #regLog('Datos a escribir: ' + str(Datos))
+        writer.writerow(Datos)
+        regLog('Datos Guardados')
+        # esperamos para realizar la proxima solicitud
+        sleep(5)
+
 # iniciamos hilo captura tiempo GPS
 threads = list()
 t = threading.Thread(target=worker)
 threads.append(t)
 t.start()
 
-# Registro suscesos en Txt y consola
-def regLog(texto):
-    print (texto)
-    fileL = open(rutaLog, "a")   # se crea el archivo 
-    fileL.write(texto + '\n') 
-    fileL.close() 
-
-# Verificacion rutas guardado archivos
-def ensure_dir(f):
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
+threads = list()
+te = threading.Thread(target=capturaEstacion)
+threads.append(te)
+te.start()
 
 try:
     
@@ -102,6 +215,7 @@ try:
     GPIO.output(ena_easy,GPIO.LOW) # Des-activamos capturado
     sleep(10)
     GPIO.output(ena_easy,GPIO.HIGH) # Activamos capturador
+
     ###################################### Inicio y Configuraciones ##################################################
     regLog("Inicio Script captura AllSky VLC")    
     GPIO.output(ena_easy,GPIO.HIGH) # Activamos capturador
@@ -147,22 +261,7 @@ try:
     ###################################### Programa principal ##################################################
     while(1):   
 
-        # Extraemos tiempo sistema
-        tiempoS[0] = int(strftime("%Y", localtime()))
-        tiempoS[1] = int(strftime("%m", localtime()))
-        tiempoS[2] = int(strftime("%d", localtime()))
-        tiempoS[3] = int(strftime("%H", localtime()))
-        tiempoS[4] = int(strftime("%M", localtime()))
-        tiempoS[5] = int(strftime("%S", localtime()))
-
-        # Tomamos el mayor de los tiempos entre Sistema y GPS
-        for i in range(0,6):
-            if tiempoS[i] < tiempoG[i]: # tiempo sistema menor que gps, actualizamos
-                tiempo = tiempoG
-                break
-            if tiempoS[i] > tiempoG[i]: # tiempo sistema mayor que gps, actualizamos
-                tiempo = tiempoS
-                break
+        tiempo = actualizarTiempo()
         regLog("Tsistema: " + str(tiempo))
 
         # Capturamos imagenes cada x tiempo
@@ -176,7 +275,8 @@ try:
         GPIO.output(led_verd,GPIO.LOW)
 
         # Creamos sistema de archivos
-        ruta = '/media/pi/4D59-20AF/fotosCieloAllSky/A' + str(tiempo[0]) + 'M' + "%02d"%tiempo[1] + 'D' + "%02d"%tiempo[2] + '/' 
+        #ruta = '/media/pi/4D59-20AF/fotosCieloAllSky/A' + str(tiempo[0]) + 'M' + "%02d"%tiempo[1] + 'D' + "%02d"%tiempo[2] + '/' 
+        ruta = '/home/pi/Desktop/fotosCieloAllSky/A' + str(tiempo[0]) + 'M' + "%02d"%tiempo[1] + 'D' + "%02d"%tiempo[2] + '/' 
         ensure_dir(ruta)
 
         # Se carga el tiempo
@@ -257,6 +357,7 @@ try:
         # Cerramos VLC
         print ("cerrando VLC... ")
         os.system("sudo killall vlc")
+
 
         
 except KeyError:
