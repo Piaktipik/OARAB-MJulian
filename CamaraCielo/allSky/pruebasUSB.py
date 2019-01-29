cdde3f9cee8ea06be0dddeda345665079dd6b93d
Julian Galvez Serna <julian.galvez.s@gmail.com>
2019-01-28 23:56:23 -0500

    Cambios Generales
    Se removieron archivos irrelevantes de pruebas anteriores.

--- CamaraCielo/allSky/pruebasUSB.py
+++
@@ -1,7 +0,0 @@
-import os
-from time import gmtime, strftime, localtime, strptime, time, sleep#, timedelta
-
-print ("Reiniciando USBs...")
-os.system("echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/unbind")
-sleep(10) 
-os.system("echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/bind")
--- CamaraCielo/allSky/usbR1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USBNAME=534d:0021
-LSUSB=$(lsusb | grep $USBNAME)
-FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
-echo $LSUSB
-echo $FOLD
-sudo ./usbreset $FOLD
